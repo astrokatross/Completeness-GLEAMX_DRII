293f88cc64d644e29491aeb92523405ae0f50081
astrokatross <kathryn.ross@icrar.org>
2026-03-19 15:43:32 +0800

    🐛 fix psf name bug

--- run_tasks.sh
+++ run_tasks.sh
@@ -34,7 +34,7 @@ fi
 mkdir "${GLEAMX}/input_images"
 
 # TODO: See how well this works with symlinks. Need to be sure the container can follow them.
-for suffix in "" "_bkg" "_rms" "_psf"
+for suffix in "" "_bkg" "_rms" "projpsf_psf"
 do
     if [[ -e "${imageset_dir}/${imageset}${suffix}.fits" ]]
     then
